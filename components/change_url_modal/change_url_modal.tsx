065a63ec34e5bd25d5f21664b90913d063a4a029
KerryAlsace <kerrydougherty@gmail.com>
2019-10-09 13:35:10 -0400

    save

--- components/change_url_modal/change_url_modal.tsx
+++ components/change_url_modal/change_url_modal.tsx
@@ -1,261 +1,280 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {Modal, OverlayTrigger, Tooltip} from 'react-bootstrap';
-import {FormattedMessage} from 'react-intl';
+import React from "react";
+import { Modal, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { FormattedMessage } from "react-intl";
 
-import Constants from 'utils/constants';
-import {getShortenedURL, cleanUpUrlable} from 'utils/url';
-import {t} from 'utils/i18n';
+import Constants from "utils/constants";
+import { getShortenedURL, cleanUpUrlable } from "utils/url";
+import { t } from "utils/i18n";
 
 type Props = {
-    {
+  /**
+   * Set whether to show the modal or not
+   */
+  show: boolean;
 
-        /**
-        * Set whether to show the modal or not
-        */
-        show: boolean;
+  /**
+   * Set to change the title of the modal
+   */
+  title?: React.ReactNode;
 
-        /**
-        * Set to change the title of the modal
-        */
-        title?: React.ReactNode;
+  /**
+   * Set to change the submit button text
+   */
+  submitButtonText?: React.ReactNode;
 
-        /**
-        * Set to change the submit button text
-        */
-        submitButtonText?: React.ReactNode;
+  /**
+   * Set to change the current URL
+   */
+  currentURL?: string;
 
-        /**
-        * Set to change the current URL
-        */
-        currentURL?: string;
+  /**
+   * Set to the current team URL
+   */
+  currentTeamURL: string;
 
-        /**
-        * Set to the current team URL
-        */
-        currentTeamURL: string;
+  /**
+   * Server error from failed channel creation
+   */
+  serverError?: React.ReactNode;
 
-        /**
-        * Server error from failed channel creation
-        */
-        serverError?: React.ReactNode;
+  /**
+   * Function to call when modal is submitted
+   */
+  onModalSubmit: () => void;
 
-        /**
-         * Function to call when modal is submitted
-         */
-        onModalSubmit: () => void;
+  /**
+   * Function to call when modal is exited
+   */
+  onModalExited?: () => void;
 
-        /**
-         * Function to call when modal is exited
-         */
-        onModalExited?:  () => void;
+  /**
+   * Function to call when modal is dimissed
+   */
+  onModalDismissed: () => void;
+};
 
-        /**
-         * Function to call when modal is dimissed
-         */
-        onModalDismissed: () => void;
+type State = {
+  currentURL: string | undefined;
+  urlError: string;
+  userEdit: boolean;
+};
+
+export default class ChangeURLModal extends React.PureComponent<Props, State> {
+  public state: State;
+  public static defaultProps: Partial<Props> = {
+    show: false,
+    title: "Change URL",
+    submitButtonText: "Save",
+    currentURL: ""
+  };
+
+  public constructor(props: Props) {
+    super(props);
+    this.state = {
+      currentURL: props.currentURL,
+      urlError: "",
+      userEdit: false
+    };
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps: Props) {
+    // eslint-disable-line camelcase
+    // This check prevents the url being deleted when we re-render
+    // because of user status check
+    if (!this.state.userEdit) {
+      this.setState({
+        currentURL: nextProps.currentURL
+      });
     }
-}
+  }
 
-export default class ChangeURLModal extends React.PureComponent<Props> {
-    public static defaultProps: Props = {
-        show: false,
-        title: 'Change URL',
-        submitButtonText: 'Save',
-        currentURL: '',
+  onURLChanged = e => {
+    const url = e.target.value.trim();
+    this.setState({
+      currentURL: url.replace(/[^A-Za-z0-9-_]/g, "").toLowerCase(),
+      userEdit: true
+    });
+  };
+
+  formattedError = (key, id, message) => {
+    return (
+      <span key={key}>
+        <FormattedMessage id={id} defaultMessage={message} />
+        <br />
+      </span>
+    );
+  };
+
+  getURLError = url => {
+    let error = []; //eslint-disable-line prefer-const
+
+    if (url.length < 2) {
+      error.push(
+        this.formattedError(
+          "error1",
+          t("change_url.longer"),
+          "URL must be two or more characters."
+        )
+      );
+    }
+    if (url.charAt(0) === "-" || url.charAt(0) === "_") {
+      error.push(
+        this.formattedError(
+          "error2",
+          t("change_url.startWithLetter"),
+          "URL must start with a letter or number."
+        )
+      );
+    }
+    if (
+      url.length > 1 &&
+      (url.charAt(url.length - 1) === "-" || url.charAt(url.length - 1) === "_")
+    ) {
+      error.push(
+        this.formattedError(
+          "error3",
+          t("change_url.endWithLetter"),
+          "URL must end with a letter or number."
+        )
+      );
+    }
+    if (url.indexOf("__") > -1) {
+      error.push(
+        this.formattedError(
+          "error4",
+          t("change_url.noUnderscore"),
+          "URL can not contain two underscores in a row."
+        )
+      );
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentURL: props.currentURL,
-            urlError: '',
-            userEdit: false,
-        };
+    // In case of error we don't detect
+    if (error.length === 0) {
+      error.push(
+        this.formattedError(
+          "errorlast",
+          t("change_url.invalidUrl"),
+          "Invalid URL"
+        )
+      );
+    }
+    return error;
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const url = this.refs.urlinput.value;
+    const cleanedURL = cleanUpUrlable(url);
+    if (cleanedURL !== url || url.length < 2 || url.indexOf("__") > -1) {
+      this.setState({ urlError: this.getURLError(url) });
+      return;
+    }
+    this.setState({ urlError: "", userEdit: false });
+    this.props.onModalSubmit(url);
+  };
+
+  onCancel = () => {
+    this.setState({ urlError: "", userEdit: false });
+    this.props.onModalDismissed();
+  };
+
+  public render() {
+    let urlClass = "input-group input-group--limit";
+    let error = null;
+
+    if (this.state.urlError) {
+      urlClass += " has-error";
     }
 
-    UNSAFE_componentWillReceiveProps(nextProps) { // eslint-disable-line camelcase
-        // This check prevents the url being deleted when we re-render
-        // because of user status check
-        if (!this.state.userEdit) {
-            this.setState({
-                currentURL: nextProps.currentURL,
-            });
-        }
+    if (this.props.serverError || this.state.urlError) {
+      error = (
+        <div className="has-error">
+          <p className="input__help error">
+            {this.state.urlError || this.props.serverError}
+          </p>
+        </div>
+      );
     }
 
-    onURLChanged = (e) => {
-        const url = e.target.value.trim();
-        this.setState({currentURL: url.replace(/[^A-Za-z0-9-_]/g, '').toLowerCase(), userEdit: true});
-    }
+    const fullURL = this.props.currentTeamURL + "/channels";
+    const shortURL = getShortenedURL(fullURL);
+    const urlTooltip = <Tooltip id="urlTooltip">{fullURL}</Tooltip>;
 
-    formattedError = (key, id, message) => {
-        return (<span key={key}>
-            <FormattedMessage
-                id={id}
-                defaultMessage={message}
-            />
-            <br/>
-        </span>);
-    }
-
-    getURLError = (url) => {
-        let error = []; //eslint-disable-line prefer-const
-
-        if (url.length < 2) {
-            error.push(
-                this.formattedError('error1', t('change_url.longer'), 'URL must be two or more characters.')
-            );
-        }
-        if (url.charAt(0) === '-' || url.charAt(0) === '_') {
-            error.push(
-                this.formattedError('error2', t('change_url.startWithLetter'), 'URL must start with a letter or number.')
-            );
-        }
-        if (url.length > 1 && (url.charAt(url.length - 1) === '-' || url.charAt(url.length - 1) === '_')) {
-            error.push(
-                this.formattedError('error3', t('change_url.endWithLetter'), 'URL must end with a letter or number.')
-            );
-        }
-        if (url.indexOf('__') > -1) {
-            error.push(
-                this.formattedError('error4', t('change_url.noUnderscore'), 'URL can not contain two underscores in a row.')
-            );
-        }
-
-        // In case of error we don't detect
-        if (error.length === 0) {
-            error.push(
-                this.formattedError('errorlast', t('change_url.invalidUrl'), 'Invalid URL')
-            );
-        }
-        return error;
-    }
-
-    onSubmit = (e) => {
-        e.preventDefault();
-        const url = this.refs.urlinput.value;
-        const cleanedURL = cleanUpUrlable(url);
-        if (cleanedURL !== url || url.length < 2 || url.indexOf('__') > -1) {
-            this.setState({urlError: this.getURLError(url)});
-            return;
-        }
-        this.setState({urlError: '', userEdit: false});
-        this.props.onModalSubmit(url);
-    }
-
-    onCancel = () => {
-        this.setState({urlError: '', userEdit: false});
-        this.props.onModalDismissed();
-    }
-
-    public render() {
-        let urlClass = 'input-group input-group--limit';
-        let error = null;
-
-        if (this.state.urlError) {
-            urlClass += ' has-error';
-        }
-
-        if (this.props.serverError || this.state.urlError) {
-            error = (
-                <div className='has-error'>
-                    <p className='input__help error'>
-                        {this.state.urlError || this.props.serverError}
-                    </p>
+    return (
+      <Modal
+        dialogClassName="a11y__modal"
+        show={this.props.show}
+        onHide={this.onCancel}
+        onExited={this.props.onModalExited}
+        role="dialog"
+        aria-labelledby="changeUrlModalLabel"
+      >
+        <Modal.Header closeButton={true}>
+          <Modal.Title componentClass="h1" id="changeUrlModalLabel">
+            {this.props.title}
+          </Modal.Title>
+        </Modal.Header>
+        <form role="form" className="form-horizontal">
+          <Modal.Body>
+            <div className="modal-intro">
+              <FormattedMessage
+                id="channel_flow.changeUrlDescription"
+                defaultMessage="Some characters are now allowed in URLs and may be removed."
+              />
+            </div>
+            <div className="form-group">
+              <label className="col-sm-3 form__label control-label">
+                <FormattedMessage
+                  id="change_url.urlLabel"
+                  defaultMessage="Channel URL"
+                />
+              </label>
+              <div className="col-sm-9">
+                <div className={urlClass}>
+                  <OverlayTrigger
+                    delayShow={Constants.OVERLAY_TIME_DELAY}
+                    placement="top"
+                    overlay={urlTooltip}
+                  >
+                    <span className="input-group-addon">{shortURL}</span>
+                  </OverlayTrigger>
+                  <input
+                    type="text"
+                    ref="urlinput"
+                    className="form-control"
+                    maxLength={Constants.MAX_CHANNELNAME_LENGTH}
+                    onChange={this.onURLChanged}
+                    value={this.state.currentURL}
+                    autoFocus={true}
+                    tabIndex="1"
+                  />
                 </div>
-            );
-        }
-
-        const fullURL = this.props.currentTeamURL + '/channels';
-        const shortURL = getShortenedURL(fullURL);
-        const urlTooltip = (
-            <Tooltip id='urlTooltip'>{fullURL}</Tooltip>
-        );
-
-        return (
-            <Modal
-                dialogClassName='a11y__modal'
-                show={this.props.show}
-                onHide={this.onCancel}
-                onExited={this.props.onModalExited}
-                role='dialog'
-                aria-labelledby='changeUrlModalLabel'
+                {error}
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button
+              type="button"
+              className="btn btn-link"
+              onClick={this.onCancel}
             >
-                <Modal.Header closeButton={true}>
-                    <Modal.Title
-                        componentClass='h1'
-                        id='changeUrlModalLabel'
-                    >
-                        {this.props.title}
-                    </Modal.Title>
-                </Modal.Header>
-                <form
-                    role='form'
-                    className='form-horizontal'
-                >
-                    <Modal.Body>
-                        <div className='modal-intro'>
-                            <FormattedMessage
-                                id='channel_flow.changeUrlDescription'
-                                defaultMessage='Some characters are now allowed in URLs and may be removed.'
-                            />
-                        </div>
-                        <div className='form-group'>
-                            <label className='col-sm-3 form__label control-label'>
-                                <FormattedMessage
-                                    id='change_url.urlLabel'
-                                    defaultMessage='Channel URL'
-                                />
-                            </label>
-                            <div className='col-sm-9'>
-                                <div className={urlClass}>
-                                    <OverlayTrigger
-                                        delayShow={Constants.OVERLAY_TIME_DELAY}
-                                        placement='top'
-                                        overlay={urlTooltip}
-                                    >
-                                        <span className='input-group-addon'>{shortURL}</span>
-                                    </OverlayTrigger>
-                                    <input
-                                        type='text'
-                                        ref='urlinput'
-                                        className='form-control'
-                                        maxLength={Constants.MAX_CHANNELNAME_LENGTH}
-                                        onChange={this.onURLChanged}
-                                        value={this.state.currentURL}
-                                        autoFocus={true}
-                                        tabIndex='1'
-                                    />
-                                </div>
-                                {error}
-                            </div>
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <button
-                            type='button'
-                            className='btn btn-link'
-                            onClick={this.onCancel}
-                        >
-                            <FormattedMessage
-                                id='change_url.close'
-                                defaultMessage='Close'
-                            />
-                        </button>
-                        <button
-                            onClick={this.onSubmit}
-                            type='submit'
-                            className='btn btn-primary'
-                            tabIndex='2'
-                        >
-                            {this.props.submitButtonText}
-                        </button>
-                    </Modal.Footer>
-                </form>
-            </Modal>
-        );
-    }
+              <FormattedMessage id="change_url.close" defaultMessage="Close" />
+            </button>
+            <button
+              onClick={this.onSubmit}
+              type="submit"
+              className="btn btn-primary"
+              tabIndex="2"
+            >
+              {this.props.submitButtonText}
+            </button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    );
+  }
 }
